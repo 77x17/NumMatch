b00f8f3c68043688945d90e17536cd57c0213650
77x17 <phanngung77x17@gmail.com>
2026-05-01 16:10:43 +0700

    [Fix] - Turn off debug Generate()

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
-        currentStage = Random.Range(0, 3);
+        currentStage = 0;
         GenerateNewStage();
     }
 
